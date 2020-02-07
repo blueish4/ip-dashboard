d51f930fd4f53f16efaa35fe38f63bf12b94a8ba
Michael Hathaway <michael@hathaways.me.uk>
2020-02-07 13:24:52 +0000

    Basic bootstrap-vue type template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
+import recent from './pages/recent.vue';
+import spectrogram from './pages/spectrogram.vue';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/recent', component: recent },
+  { path: '/spectrogram', component: spectrogram },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes, // short for `routes: routes`
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
