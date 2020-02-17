1e0956ad518bf4e1fb3130427cad39af7fd6bd17
blueish <blueish4@users.noreply.github.com>
2020-02-17 12:29:59 +0000

    demo for rendering new chart with chart.js

--- src/pages/recent.vue
+++ src/pages/recent.vue
@@ -1,11 +1,34 @@
 <template>
-  <p>Hello world</p>
+  <div>
+    <dbGraph
+      v-if="loaded"
+      :chartData="chartData"
+      :options="options"
+    />
+    <p>Last seen at {{ date }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import dbGraph from '../components/dbGraph.vue';
+
 
 export default Vue.extend({
+  components: {
+    dbGraph,
+  },
+  data: () => ({
+    loaded: false,
+    chartData: null,
+    date: new Date(),
+  }),
+  async mounted() {
+    const res = await fetch('https://europe-west1-individual-project-265621.cloudfunctions.net/get-latest-frequencies');
+    const json = await res.json();
+    this.date = new Date(json.timestamp.seconds);
+    console.log(json);
+  },
 });
 </script>
 
