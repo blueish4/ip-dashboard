b34c42d96b0686103a77caf0a7f12206d4d188ce
Michael Hathaway <michael@hathaways.me.uk>
2020-02-17 12:29:59 +0000

    demo for rendering new chart with chart.js

---
+++ src/components/dbGraph.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Vue from 'vue';
+import { Bar, mixins } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart(this.chartData, this.options);
+  },
+};
+</script>
