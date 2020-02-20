b8ece9443702bac5ade3cc026a418d9e7e476b9f
blueish <blueish4@users.noreply.github.com>
2020-02-20 00:21:47 +0000

    init vuex for storage of objects

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,11 +11,24 @@ import spectrogram from './pages/spectrogram.vue';
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const routes = [
   { path: '/recent', component: recent },
   { path: '/spectrogram', component: spectrogram },
 ];
+
+// TODO static typing for record
+const store = new Vuex.Store({
+  state: {
+    records: [] as any[],
+  },
+  mutations: {
+    importNewData(state, record: any) {
+      state.records.push(record);
+    },
+  },
+});
 
 const router = new VueRouter({
   mode: 'history',
